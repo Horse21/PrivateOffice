384831e52bfd4acf75eda64f32477841dd22c877
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-09-20 17:09:01 +0300

    #1200 дополнительные поля

--- src/app/components/trip-request/trip-request.component.ts
+++ src/app/components/trip-request/trip-request.component.ts
@@ -37,6 +37,12 @@ export class TripRequestComponent {
 		form.append("userName", this.getCookie("userName"));
 		form.append("password", this.getCookie("password"));
 
+		form.append("Firstname", this.travelerFirstNameControl.value);
+		form.append("Lastname", this.travelerLastNameControl.value);
+		form.append("Destination", this.destinationControl.value);
+		form.append("ArrivalDate", this.arrivalDate.toJSON());
+		form.append("DepartureDate", this.departureDate.toJSON());
+
 		let headers = new HttpHeaders();
 		headers.set("Content-Type", "application/x-www-form-urlencoded");
 		headers.set("Accept", "application/json");
